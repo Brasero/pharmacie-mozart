5eb54107c6ddda431352d9d4ecc9a00fa7b1bfc9
Brasero <brandonricci4@gmail.com>
2022-05-31 14:35:40 +0200

    building ios from HP

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -55,7 +55,7 @@ export class ApiService {
       obj.append('generique', g);
       //Filling formData object with saved photos
       this.photo.photos.map((value, index) => {
-        obj.append('image' + index, value.webviewPath);
+        obj.append('image', value.webviewPath);
       });
       return obj;
     } else {
